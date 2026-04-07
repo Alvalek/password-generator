23eacf6088b90ade8e4c154fda4095874fdb9f0b
Alvalek <wacicic@proton.me>
2026-04-06 23:02:34 -0300

    main.py

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 import random
 import string
-def generate_password(size=12):
-  characters = string.ascii_letters + string.digits + string.punctuation
-  password = ".join(random.choice(characters) for_in
-range(size))
-    return password
 
-print("Password generated:", generate_password())
+def generate_password(size=12):
+    characters = string.ascii_letters + string.digits + string.punctuation
+    password = ''.join(random.choice(characters) for _ in range(size))
+    return password
+print("Generated password:", generate_password())
